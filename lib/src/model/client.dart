13ea718b27d50fab96b5424ce8edab2c06e0e3b3
Bá Nguyễn <luutru.banguyen@gmail.com>
2021-11-28 17:58:07 +0700

    client

--- lib/src/model/client.dart
+++ lib/src/model/client.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_my_train/src/model/otd/book.dart';
+import 'package:flutter_my_train/src/model/otd/user.dart';
 
 class ClientModel extends ChangeNotifier {
   BookOTD book;
+  BookOTD bookIndex;
+  UserOTD user;
 
-  void changeBook(BookOTD bookOTD) {
-    book = bookOTD;
+  void changeUser(UserOTD user) {
+    this.user = user;
+    notifyListeners();
+  }
+
+  void changeBook(BookOTD book) {
+    this.book = book;
+    notifyListeners();
+  }
+
+  void changeBookIndex(BookOTD bookIndex) {
+    this.bookIndex = bookIndex;
     notifyListeners();
   }
 }
